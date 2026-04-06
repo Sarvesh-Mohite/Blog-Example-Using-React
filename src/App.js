ef31c380a529a8b9c04ca21c5ed6da40b1e75689
Sarvesh Mohite <mohite.sarvesh20@gmail.com>
2026-04-06 22:13:46 +0530

    version 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom' // Changed Switch to Routes
+import NotFound from './components/NotFound'
+import Header from './components/Header'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+    <Header />
+    <Routes> {/* Changed Switch to Routes */}
+      {/* Updated component to element and removed exact */}
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      
+      {/* NotFound is now a path="*" catch-all */}
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
+)
 
-export default App;
+export default App
